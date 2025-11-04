4a1f8ee6a1af95df35366e43b6151dd29f4bff4a
paulgreen920-del <paulgreen920@gmail.com>
2025-11-04 12:28:04 -0500

    fix(postcss): use tailwindcss+autoprefixer plugin config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-module.exports = {
+﻿module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
