708127fec76f336d66bb29c7b40a034b37581d9e
paulgreen920-del <paulgreen920@gmail.com>
2025-11-11 01:36:00 -0500

    Remove problematic availability page to unblock deployment

--- app/onboarding/availability-disabled/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-  return <div className="max-w-3xl mx-auto p-6">Loading...</div>;
-}
--- app/onboarding/availability-disabled/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export const dynamic = "force-dynamic";
-
-import AvailabilityForm from "./AvailabilityForm";
-
-export default function AvailabilityPage() {
-  return <AvailabilityForm />;
-}
