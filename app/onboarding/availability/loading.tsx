c5657066fe6d253ad9f32d3d6bd13310e995e617
paulgreen920-del <paulgreen920@gmail.com>
2025-11-11 01:21:30 -0500

    Add loading state to fix prerender issue

---
+++ app/onboarding/availability/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div className="max-w-3xl mx-auto p-6">Loading...</div>;
+}
--- app/onboarding/availability/page.tsx
+++ app/onboarding/availability/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
-import AvailabilityForm from "./AvailabilityForm";
+"use client";
 
 export const dynamic = "force-dynamic";
 
+import AvailabilityForm from "./AvailabilityForm";
+
 export default function AvailabilityPage() {
-  return (
-    <Suspense fallback={<div className="max-w-3xl mx-auto p-6">Loading...</div>}>
-      <AvailabilityForm />
-    </Suspense>
-  );
+  return <AvailabilityForm />;
 }
